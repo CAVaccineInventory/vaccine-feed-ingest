f833b59924daa7470a31d1a0123d95b557b8dcf2
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-04-19 20:18:18 +0000

    Add import source locations schema

--- vaccine_feed_ingest/schema/schema.py
+++ vaccine_feed_ingest/schema/schema.py
@@ -180,3 +180,22 @@ class NormalizedLocation(BaseModel):
     notes: Optional[List[str]]
     active: Optional[bool]
     source: Source
+
+
+class ImportMatchAction(BaseModel):
+    """Match action to take when importing a source location"""
+
+    id: Optional[str]
+    action: str
+
+
+class ImportSourceLocation(BaseModel):
+    """Import source location record"""
+
+    source_uid: str
+    source_name: str
+    name: Optional[str]
+    latitude: Optional[float]
+    longitude: Optional[float]
+    import_json: dict
+    match: Optional[ImportMatchAction]
