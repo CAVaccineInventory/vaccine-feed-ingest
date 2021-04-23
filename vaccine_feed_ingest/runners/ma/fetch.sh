2db64b92e085008098a698e540eeed7bbbf327fc
Deon Hua <dhua@slack-corp.com>
2021-04-23 13:23:09 -0700

    Add Fetch + Parse steps for Mass.

---
+++ vaccine_feed_ingest/runners/ma/fetch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+output_dir=""
+
+if [ -n "${1}" ]; then
+    output_dir="${1}"
+else
+    echo "Must pass an output_dir as first argument"
+fi
+
+cd "$output_dir"
+
+curl --silent "https://www.mass.gov/doc/covid-19-vaccine-locations-for-currently-eligible-recipients-csv/download" -o 'ma.xlsx'
---
+++ vaccine_feed_ingest/runners/ma/parse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import json
+import pandas
+import pathlib
+import sys
+import csv
+
+output_dir = pathlib.Path(sys.argv[1])
+input_dir = pathlib.Path(sys.argv[2])
+
+csv_filepaths = input_dir.glob("*.xlsx")
+
+for in_filepath in csv_filepaths:
+    locations = []
+    data = pandas.read_excel(in_filepath, engine="openpyxl")
+
+    for row in data.itertuples():
+        locations.append(dict(name=row[1], address=row[3]))
+
+    out_filepath = output_dir + "/data.parsed.ndjson"
+
+    with out_filepath.open("w") as fout:
+        for location in locations:
+            json.dump(location, fout)
+            fout.write("\n")
