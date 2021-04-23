dc4df5fa3d975b585cbeceb8c358ccb0fbfa1789
juleea <hi.juleea@gmail.com>
2021-04-23 15:21:25 -0700

    linter

--- vaccine_feed_ingest/runners/us/vaccinespotter_org/parse.py
+++ vaccine_feed_ingest/runners/us/vaccinespotter_org/parse.py
@@ -5,14 +5,14 @@ import pathlib
 import sys
 
 input_dir = pathlib.Path(sys.argv[2])
-input_file = input_dir / 'data.geojson'
+input_file = input_dir / "data.geojson"
 output_dir = pathlib.Path(sys.argv[1])
-output_file = output_dir / 'data.parsed.ndjson'
+output_file = output_dir / "data.parsed.ndjson"
 
 with input_file.open() as fh:
     raw_json = json.load(fh)
 
-with output_file.open('w') as fout:
-    for feature in raw_json['features']:
+with output_file.open("w") as fout:
+    for feature in raw_json["features"]:
         json.dump(feature, fout)
         fout.write("\n")
