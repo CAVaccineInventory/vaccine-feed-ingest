eff91c2fb8518d8bb7bbe851cf59a8bc9ad5ef1a
Deon Hua <dhua@slack-corp.com>
2021-04-23 15:00:24 -0700

    Cleanup

--- vaccine_feed_ingest/runners/ma/parse.py
+++ vaccine_feed_ingest/runners/ma/parse.py
@@ -9,18 +9,17 @@ import pandas
 output_dir = pathlib.Path(sys.argv[1])
 input_dir = pathlib.Path(sys.argv[2])
 
-csv_filepaths = input_dir.glob("*.xlsx")
+xlsx_filepath = input_dir / "ma.xlsx"
 
-for in_filepath in csv_filepaths:
-    locations = []
-    data = pandas.read_excel(in_filepath, engine="openpyxl")
+locations = []
+data = pandas.read_excel(xlsx_filepath, engine="openpyxl")
 
-    for row in data.itertuples():
-        locations.append(dict(name=row[1], address=row[3]))
+for row in data.itertuples():
+    locations.append(dict(name=row[1], address=row[3]))
 
-    out_filepath = output_dir + "/data.parsed.ndjson"
+out_filepath = output_dir / "data.parsed.ndjson"
 
-    with out_filepath.open("w") as fout:
-        for location in locations:
-            json.dump(location, fout)
-            fout.write("\n")
+with out_filepath.open("w") as fout:
+    for location in locations:
+        json.dump(location, fout)
+        fout.write("\n")
