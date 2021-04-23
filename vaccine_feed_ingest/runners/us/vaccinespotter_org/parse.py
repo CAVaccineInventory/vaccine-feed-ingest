c9eb548247f367120b39607fccb91f7730393793
juleea <hi.juleea@gmail.com>
2021-04-23 15:08:03 -0700

    single file

--- vaccine_feed_ingest/runners/us/vaccinespotter_org/parse.py
+++ vaccine_feed_ingest/runners/us/vaccinespotter_org/parse.py
@@ -4,19 +4,15 @@ import json
 import pathlib
 import sys
 
-output_dir = pathlib.Path(sys.argv[1])
 input_dir = pathlib.Path(sys.argv[2])
+input_file = input_dir / 'data.geojson'
+output_dir = pathlib.Path(sys.argv[1])
+output_file = output_dir / 'data.parsed.ndjson'
 
-json_filepaths = input_dir.glob("*.geojson")
+with input_file.open() as fh:
+    raw_json = json.load(fh)
 
-for in_filepath in json_filepaths:
-    with in_filepath.open() as fin:
-        raw_json = json.load(fin)
-
-    filename = in_filepath.name.split(".", maxsplit=1)[0]
-    out_filepath = output_dir / f"{filename}.parsed.ndjson"
-
-    with out_filepath.open("w") as fout:
-        for site in raw_json["features"]:
-            json.dump(site, fout)
-            fout.write("\n")
+with output_file.open('w') as fout:
+    for feature in raw_json['features']:
+        json.dump(feature, fout)
+        fout.write("\n")
