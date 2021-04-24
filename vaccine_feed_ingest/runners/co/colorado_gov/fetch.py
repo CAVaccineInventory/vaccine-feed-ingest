31fa699ffe8d51d800952899c764ae7cbd9c670c
Matt Bacchi <mbacchi@gmail.com>
2021-04-24 15:59:10 -0400

    Add Colorado fetcher

---
+++ vaccine_feed_ingest/runners/co/colorado_gov/fetch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pathlib
+import sys
+import requests
+
+output_dir = pathlib.Path(sys.argv[1])
+output_file = output_dir / "colorado_gov.html"
+
+with output_file.open("w") as fout:
+    r = requests.get(
+        "https://covid19.colorado.gov/vaccine/where-you-can-get-vaccinated"
+    )
+
+    fout.write(r.text)
+    fout.write("\n")
