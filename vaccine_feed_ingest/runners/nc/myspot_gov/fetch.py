bc015c62ebf27b380050ff2f48f96688ec8aff9e
Ran Reichman <ran@embarktrucks.com>
2021-04-24 09:02:46 -0700

    lint fixes

--- vaccine_feed_ingest/runners/nc/myspot_gov/fetch.py
+++ vaccine_feed_ingest/runners/nc/myspot_gov/fetch.py
@@ -1,6 +1,8 @@
-### This script uses Selenium to collect data from the NC official website, by querying per NC zip code. The zip codes are collected through
-### the requests library via a third party website. The script takes about 4 minutes to run on my personal computer and produces an html file
-### for each zip code.
+'''
+This script uses Selenium to collect data from the NC official website, by querying per NC zip code. The zip codes are collected through
+the requests library via a third party website. The script takes about 4 minutes to run on my personal computer and produces an html file
+for each zip code.
+'''
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -13,6 +15,7 @@ import re
 import requests
 import sys
 
+
 def connect_to_website(website_link):
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
@@ -22,8 +25,9 @@ def connect_to_website(website_link):
 
     timeout_in_secs = 20
     try:
-        WebDriverWait(driver, timeout_in_secs).until(expected_conditions.invisibility_of_element_located((By.XPATH, '/html/body/app-root/ngx-spinner/div/div[1]')))
-    except NoSuchElementException as e:
+        WebDriverWait(driver, timeout_in_secs).until(
+            expected_conditions.invisibility_of_element_located((By.XPATH, '/html/body/app-root/ngx-spinner/div/div[1]')))
+    except NoSuchElementException:
         # Spinner element is no longer there, leading to an exception being thrown.
         pass
     # The website loads with a spinner and doesn't allow vaccine location querying while the spinner is loading
@@ -62,7 +66,7 @@ def write_data_html_to_file(filename, data_html):
 # Default website includes conveniently styled information on North Carolina zip codes.
 def get_nc_zip_codes(zip_code_website="https://www.zip-codes.com/state/nc.asp"):
     nc_zip_codes_html_data = requests.get(zip_code_website)
-    zip_code_regex = "ZIP[ ]Code[ ]\d{5}"
+    zip_code_regex = "ZIP[ ]Code[ ]\\d{5}"
     zip_codes_as_strings = re.findall(zip_code_regex, nc_zip_codes_html_data.text)
     unique_zip_codes_as_strings = list(set([x.replace("ZIP Code ", "") for x in zip_codes_as_strings]))
     unique_zip_codes_as_strings.sort()
@@ -82,4 +86,3 @@ for nc_zip_code in nc_zip_codes:
     data_html = get_data_html_from_website_driver(nc_driver, nc_zip_code)
     output_filename = os.path.join(output_directory, nc_zip_code + ".html")
     write_data_html_to_file(output_filename, data_html)
-
