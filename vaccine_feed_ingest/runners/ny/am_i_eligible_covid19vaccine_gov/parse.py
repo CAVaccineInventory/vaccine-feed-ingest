b0d3612c8aaa5aa37d45baf240b611111ad0dcec
David Bonner <bonnerd@media.mit.edu>
2021-04-24 14:02:52 -0400

    add normalizer for am_i_eligible_covid19vaccine_gov (#113)
    * add normalizer for am_i_eligible_covid19vaccine_gov
    
    also updates the parser to insert the feed-level `lastUpdated` field into
    each site.
    
    * let's try to make the linter happy
    
    * updates to ny am_i_eligible_covid19vaccine_gov normalizer per PR
    
    * use the schema for what we can.  the addresses they return won't validate,
      for lack of street/zip, so keep doing that by hand.
    * pull in pytz to set the (assumed ET) tz for the `lastUpdated` field
    
    * make isort linter happy

--- vaccine_feed_ingest/runners/ny/am_i_eligible_covid19vaccine_gov/parse.py
+++ vaccine_feed_ingest/runners/ny/am_i_eligible_covid19vaccine_gov/parse.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import datetime
 import json
 import pathlib
 import sys
+
+import pytz
 
 input_dir = pathlib.Path(sys.argv[2])
 output_dir = pathlib.Path(sys.argv[1])
@@ -14,8 +17,13 @@ output_file = output_dir / "data.parsed.ndjson"
 with input_file.open() as fin:
     data = json.load(fin)
 
+eastern = pytz.timezone("US/Eastern")
+last_updated = eastern.localize(
+    datetime.datetime.strptime(data["lastUpdated"], "%m/%d/%Y, %I:%M:%S %p")
+).isoformat()
 
 with output_file.open("w") as fout:
     for site in data["providerList"]:
+        site["lastUpdated"] = last_updated
         json.dump(site, fout)
         fout.write("\n")
