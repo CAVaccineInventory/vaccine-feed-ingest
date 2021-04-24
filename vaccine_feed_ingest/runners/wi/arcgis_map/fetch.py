91b72897e89da6762c323f0dbb25a0fdc44dc7b5
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-23 21:09:00 -0700

    add check for output filename like bash script

--- vaccine_feed_ingest/runners/wi/arcgis_map/fetch.py
+++ vaccine_feed_ingest/runners/wi/arcgis_map/fetch.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 from arcgis.features import FeatureLayer
+import sys
 
 url = "https://dhsgis.wi.gov/server/rest/services/DHS_COVID19/COVID19_Vaccine_Provider_Sites/MapServer/0"
 
+
+output_dir = sys.argv[1]
+if output_dir is None:
+    print("Must pass an output_dir as first argument")
+
 layer = FeatureLayer(url)
 results = layer.query(return_all_records=True)
-results.save('/tmp', 'wi_arcgis_map.json')
+results.save(output_dir, 'wi_arcgis_map.json')
