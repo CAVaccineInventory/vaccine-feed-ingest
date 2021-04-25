0ac390ebebb8631451d169d8d1abcd9edaae8543
Ran Reichman <ran.reichman@gmail.com>
2021-04-24 21:51:51 -0700

    Added fetch for nc data (#131)
    Co-authored-by: Ran Reichman <ran@embarktrucks.com>

---
+++ vaccine_feed_ingest/runners/nc/myspot_gov/fetch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+
+import requests
+
+output_directory = sys.argv[1]
+
+nc_vaccine_info_website_api = "https://myspot.nc.gov/api/get-vaccine-locations"
+nc_vaccine_data = requests.get(nc_vaccine_info_website_api).json()
+nc_vaccine_data_output_filename = "nc_data.json"
+
+with open(
+    os.path.join(output_directory, nc_vaccine_data_output_filename), "w"
+) as outfile:
+    json.dump(nc_vaccine_data, outfile)
