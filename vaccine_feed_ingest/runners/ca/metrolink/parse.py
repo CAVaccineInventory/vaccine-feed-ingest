3a09c6395671267a82b620a3dd2d72412c7b4dc1
Ross Rheingans-Yoo <ross@r-y.io>
2021-04-26 01:49:53 -0400

    format

--- vaccine_feed_ingest/runners/ca/metrolink/parse.py
+++ vaccine_feed_ingest/runners/ca/metrolink/parse.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import pathlib
 import sys
-import os
 
 from bs4 import BeautifulSoup
 
@@ -14,24 +14,26 @@ input_filenames = [p for p in pathlib.Path(input_dir).iterdir() if p.is_file()]
 
 for filename in input_filenames:
     sites = []
-    
+
     with filename.open() as fin:
         content = fin.read()
-    
+
     soup = BeautifulSoup(content, "html.parser")
-    table = soup.find(id='vaxLocationsTable')
+    table = soup.find(id="vaxLocationsTable")
     for row in table.find("tbody").find_all("tr"):
         cells = row.find_all("td")
-        
+
         site = {
-            "site_name": str(cells[0].renderContents())[2:-1].split(' <br/> ')[0],
-            "site_address": str(cells[0].renderContents())[2:-1].split(' <br/> ')[1],
+            "site_name": str(cells[0].renderContents())[2:-1].split(" <br/> ")[0],
+            "site_address": str(cells[0].renderContents())[2:-1].split(" <br/> ")[1],
             "metrolink_line": cells[1].string,
             "metrolink_station": cells[2].string,
         }
         sites.append(site)
-    
-    with (output_dir / (os.path.basename(filename) + '.parsed.ndjson')).open('w') as fout:
+
+    with (output_dir / (os.path.basename(filename) + ".parsed.ndjson")).open(
+        "w"
+    ) as fout:
         for site in sites:
             json.dump(site, fout)
-            fout.write('\n')
+            fout.write("\n")
