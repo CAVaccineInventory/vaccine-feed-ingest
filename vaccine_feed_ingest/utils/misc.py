40817d046d1d12b764159c0adabc28d373984dde
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-04-26 04:25:07 +0000

    Simplify batch util

--- vaccine_feed_ingest/utils/misc.py
+++ vaccine_feed_ingest/utils/misc.py
@@ -4,13 +4,13 @@ from typing import Iterable, Iterator
 
 
 def batch(iterable: Iterable, size: int) -> Iterator[Iterator]:
-    """Batch an interable into chunks of specified size"""
+    """Batch an iterable into chunks of specified size"""
     iterator = iter(iterable)
+
     while True:
-        batch_iterator = itertools.islice(iterator, size)
-        try:
-            yield itertools.chain([next(batch_iterator)], batch_iterator)
-        except StopIteration:
-            # As of PEP 479 (released in python 3.7), cannot raise StopIteration
-            # from inside a generator.
+        batch_iterator = list(itertools.islice(iterator, size))
+
+        if not batch_iterator:
             return
+
+        yield iter(batch_iterator)
