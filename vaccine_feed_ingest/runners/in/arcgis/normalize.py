c1f8f89223eeaa91e632f0388b7e0704d6e25850
Eli Block <3347571+eliblock@users.noreply.github.com>
2021-04-26 21:20:57 -0700

    fix: vaccine type parsing is lowercase

--- vaccine_feed_ingest/runners/in/arcgis/normalize.py
+++ vaccine_feed_ingest/runners/in/arcgis/normalize.py
@@ -40,10 +40,11 @@ def _get_inventory(site: dict) -> Optional[List[schema.Vaccine]]:
     vaccines_field = site["attributes"]["Vaccine_Type"].lower().split(",")
 
     potentials = {
-        "Pfizer": schema.Vaccine(vaccine="pfizer_biontech"),
-        "Moderna": schema.Vaccine(vaccine="moderna"),
-        "Johnson & Johnson": schema.Vaccine(vaccine="johnson_johnson_janssen"),
-        "FRPP": None,
+        "pfizer": schema.Vaccine(vaccine="pfizer_biontech"),
+        "moderna": schema.Vaccine(vaccine="moderna"),
+        "johnson & johnson": schema.Vaccine(vaccine="johnson_johnson_janssen"),
+        "frpp": None,
+        "multi": None,
     }
 
     inventory = []
