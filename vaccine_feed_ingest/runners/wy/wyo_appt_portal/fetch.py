15298c37d169635c8bbd6863883e9e29b8681b6c
Matthew Sachs <matthewg@zevils.com>
2021-04-27 21:44:53 -0700

    Implement fetch wy/wyo_appt_portal (closes #244)

---
+++ vaccine_feed_ingest/runners/wy/wyo_appt_portal/fetch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import requests
+
+base_url = "https://www.wyoapptportal.org/clinic/search"
+
+output_dir = sys.argv[1]
+if output_dir is None:
+    print("Must pass an output_dir as first argument")
+
+page = 1
+while True:
+    url = "{}?page={}".format(base_url, page)
+    response = requests.get(url, allow_redirects=False)
+
+    # when out of results, will return 302
+    if response.status_code != 200:
+        break
+
+    with open(os.path.join(output_dir, "{}.html".format(page)), "w") as f:
+        f.write(response.text)
+
+    page += 1
