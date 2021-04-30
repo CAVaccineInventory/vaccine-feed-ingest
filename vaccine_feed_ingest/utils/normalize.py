0bca56c19a180c542719d882ec78c15de074c1f6
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-04-29 17:15:43 -0700

    Upgrade dependancies to latest (#294)
    * Upgrade dependancies to latest
    
    * Fix spaces
    
    * Fix spaces

--- vaccine_feed_ingest/utils/normalize.py
+++ vaccine_feed_ingest/utils/normalize.py
@@ -6,7 +6,7 @@ from typing import Optional, Tuple
 
 
 def provider_id_from_name(name: str) -> Optional[Tuple[str, str]]:
-    """ Generate provider ids for retail pharmacies (riteaid:123) """
+    """Generate provider ids for retail pharmacies (riteaid:123)"""
 
     m = re.search(r"RITE AID PHARMACY (\d+)", name, re.I)
     if m:
