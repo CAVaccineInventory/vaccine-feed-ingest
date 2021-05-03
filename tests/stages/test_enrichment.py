15f1f34f4988e9c68d206a5ac1f53daf1b9ec9de
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-05-03 16:19:56 -0700

    Add concordance link for source if it is missing (#359)

--- tests/stages/test_enrichment.py
+++ tests/stages/test_enrichment.py
@@ -17,3 +17,14 @@ def test_add_provider_from_name(full_location):
 
     assert full_location.parent_organization
     assert str(full_location.parent_organization.id) == "rite_aid"
+
+
+def test_add_source_link(full_location):
+    # Clear parent prganization to check
+    full_location.links = None
+
+    enrichment._add_source_link(full_location)
+
+    links = enrichment._generate_link_map(full_location)
+    assert full_location.source.source in links
+    assert links[full_location.source.source] == full_location.source.id
