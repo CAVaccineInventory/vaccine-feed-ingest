af3c192366b98ebe6b9af1277db4b6ee91fbaf19
skbdpup <skbdpup@gmail.com>
2021-05-04 09:37:45 -0400

    Let curl print diagnostic messages. #368

--- vaccine_feed_ingest/runners/wv/dhhr/fetch.sh
+++ vaccine_feed_ingest/runners/wv/dhhr/fetch.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+set -Eeu -o pipefail
 
 output_dir=""
 if [ -n "${1}" ]; then
     output_dir="${1}"
 else
-    echo "Must pass an output_dir as first argument"
+    echo >&2 "Must pass an output_dir as first argument"
+    exit 1
 fi
 
-(cd "$output_dir" && curl --silent "https://dhhr.wv.gov/News/2021/Pages/COVID-19-Vaccination-Clinics-March-2-7,-2021.aspx" -o 'dhhr.html')
+cd "$output_dir"
+curl --no-progress-meter 'https://dhhr.wv.gov/News/2021/Pages/COVID-19-Vaccination-Clinics-March-2-7,-2021.aspx' -o 'dhhr.html'
