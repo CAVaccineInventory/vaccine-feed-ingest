e71408d38a48404f0a68a436c23743203fdfe2a2
davekasten <dave.kasten@gmail.com>
2021-05-15 12:09:46 -0400

    Update fetch.sh

--- vaccine_feed_ingest/runners/us/va_gov/fetch.sh
+++ vaccine_feed_ingest/runners/us/va_gov/fetch.sh
@@ -10,6 +10,10 @@ else
     echo "Must pass an output_dir as first argument"
 fi
 
-# Per VA_gov spec
-(cd "$output_dir" && curl -X GET 'https://sandbox-api.va.gov/services/va_facilities/v0/facilities/all' \
---header "apikey: ${TOKEN_FETCH_US_VA}" --create-dirs -o 'va_gov.json')
+# Per VA_gov spec available at https://developer.va.gov/explore/facilities/docs/facilities?version=current
+(cd "$output_dir" && curl -X GET 'https://sandbox-api.va.gov/services/va_facilities/v0/facilities/all' \  
+--header "apikey: ${TOKEN_FETCH_US_VA}" --create-dirs -o 'va_gov.json')  ##takes an API key that you can self-service create from https://developer.va.gov/apply -- or I can provide
+##this secret out-of-band via Discord or whatever
+
+##Note: Sandbox appears to be appropriate environment for read-only access; it appears prod is only for authorized developers that are building things that 
+##can change state on the VA server side.  Data is accurate as far as I can tell against what's available in real time on VA website.
