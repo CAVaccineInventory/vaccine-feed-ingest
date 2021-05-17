bdabf848660d28745cb89ece0c3fa65ef67dc18e
davekasten <dave.kasten@gmail.com>
2021-05-17 10:21:05 -0400

    Add files via upload

--- vaccine_feed_ingest/runners/us/va_gov/fetch.sh
+++ vaccine_feed_ingest/runners/us/va_gov/fetch.sh
@@ -10,5 +10,5 @@ else
     echo "Must pass an output_dir as first argument"
 fi
 
-# Uses official VA API for searching for locations.  NOTE: underlying data source may not include some VA pop up vaccination events at smaller VA clinics or locations like VFW halls.
+# Per VA_gov spec
 (cd "$output_dir" && curl -X GET "https://api.va.gov/v1/facilities/va?bbox%5B%5D=-180&bbox%5B%5D=-90&bbox%5B%5D=180&bbox%5B%5D=90&type=health&services%5B%5D=Covid19Vaccine&page=1&per_page=2000&radius=25000&latitude=37.408123149415275&longitude=-93.14343299172322" --create-dirs -o 'va_gov.json')
