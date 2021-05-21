fbd9fdd7cf90e97ab85ec6aeb17261e6d947f487
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-20 21:46:20 -0700

    run tox

--- vaccine_feed_ingest/runners/us/giscorps_vaccine_providers/normalize.py
+++ vaccine_feed_ingest/runners/us/giscorps_vaccine_providers/normalize.py
@@ -6,8 +6,8 @@ import os
 import pathlib
 import sys
 from typing import List, Optional
-from parse_opening_hours import OpeningHours
 
+from parse_opening_hours import OpeningHours
 from vaccine_feed_ingest_schema import location as schema
 
 from vaccine_feed_ingest.utils.log import getLogger
