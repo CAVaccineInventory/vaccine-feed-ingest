56adcf9b018014a946a899f0ea6854fa30e6453d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-28 13:55:05 -0700

    add function to normalize state names

--- vaccine_feed_ingest/runners/us/giscorps_vaccine_providers/normalize.py
+++ vaccine_feed_ingest/runners/us/giscorps_vaccine_providers/normalize.py
@@ -175,6 +175,85 @@ def try_get_lat_long(site):
     return location
 
 
+def normalize_state_name(name: str) -> str:
+
+    if name is None:
+        return name
+
+    us_state_abbrev = {
+        'Alabama': 'AL',
+        'Alaska': 'AK',
+        'American Samoa': 'AS',
+        'Arizona': 'AZ',
+        'Arkansas': 'AR',
+        'California': 'CA',
+        'Colorado': 'CO',
+        'Connecticut': 'CT',
+        'Delaware': 'DE',
+        'District of Columbia': 'DC',
+        'Florida': 'FL',
+        'Georgia': 'GA',
+        'Guam': 'GU',
+        'Hawaii': 'HI',
+        'Idaho': 'ID',
+        'Illinois': 'IL',
+        'Indiana': 'IN',
+        'Iowa': 'IA',
+        'Kansas': 'KS',
+        'Kentucky': 'KY',
+        'Louisiana': 'LA',
+        'Maine': 'ME',
+        'Maryland': 'MD',
+        'Massachusetts': 'MA',
+        'Michigan': 'MI',
+        'Minnesota': 'MN',
+        'Mississippi': 'MS',
+        'Missouri': 'MO',
+        'Montana': 'MT',
+        'Nebraska': 'NE',
+        'Nevada': 'NV',
+        'New Hampshire': 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Northern Mariana Islands': 'MP',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Puerto Rico': 'PR',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virgin Islands': 'VI',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY'
+    }
+
+    name = name.strip()
+    name = name.replace(".", "")
+
+    #capitalize the first letter of each word in cases where a state name is provided
+    spl = name.split(" ")
+    if len(spl) > 1:
+        " ".join([word.capitalize() for word in spl])
+    else:
+        name = name.lower().capitalize()
+
+    if name in us_state_abbrev:
+        return us_state_abbrev[name]
+
+    return name.upper()
+
 def _get_address(site):
 
     parsed = parse_address(site["attributes"]["fulladdr"])
