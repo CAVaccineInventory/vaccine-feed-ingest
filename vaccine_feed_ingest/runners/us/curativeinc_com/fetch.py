714bc8cccbc7fc586c496666c621b4f93575b08c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-06-03 11:03:42 -0700

    rewrite to use async io and the provided list of site ids from the API

--- vaccine_feed_ingest/runners/us/curativeinc_com/fetch.py
+++ vaccine_feed_ingest/runners/us/curativeinc_com/fetch.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 
-import json
+import asyncio
 import logging
 import pathlib
 import sys
 
-import urllib3
+from aiohttp import ClientSession
 
 output_dir = pathlib.Path(sys.argv[1])
 base_url = "https://labtools.curativeinc.com/api/v1/testing_sites"
 
-# Configure urllib3
-retries = urllib3.util.Retry(status=5, backoff_factor=2, status_forcelist=[500])
-http = urllib3.PoolManager(retries=retries)
 
 # Configure logger
 logging.basicConfig(
@@ -23,48 +20,55 @@ logging.basicConfig(
 logger = logging.getLogger("curative")
 
 
-# Keep track of site_ids that are not found
-not_found = set()
+# def fetch_ids():
+#     # fetching the base url returns a list of sites and their ids
+#     #make the request
+#     r = http.request("GET", base_url)
+#     #get the response and decode it, assuming utf8
+#     obj = json.loads(r.data.decode("utf-8"))
+
+#     valid_ids = [o.get("id") for o in obj]
+#     # valid_ids.sort()
+#     return valid_ids
 
 
-def out_of_range(site_id, run=200):
+def generate_url_for(site_id):
     """
-    Handle large gaps in curative data.
-
-    Returns True if the previous {run} sites are not found
+    generate and return a tuple containing the next curative url and the output destination for that file
     """
-    if len(not_found) < run:
-        return False
-
-    num_not_found = 0
-    for i in range(site_id - 1, site_id - run - 1, -1):
-        if i in not_found:
-            num_not_found += 1
-        else:
-            return False
-
-    if num_not_found != run:
-        return False
-
-    return True
-
-
-site_id = 1
-while True:
     url = f"{base_url}/{site_id}"
-    r = http.request("GET", url)
-
-    obj = json.loads(r.data.decode("utf-8"))
-    if "error_code" in obj:
-        error_msg = obj.get("error_message", "")
-        logger.info(error_msg)
-        if error_msg.startswith("No site found"):
-            if out_of_range(site_id):
-                break
-            not_found.add(site_id)
-        site_id += 1
-        continue
-
     output_file = output_dir / f"{site_id}.json"
-    output_file.write_bytes(r.data)
-    site_id += 1
+    return (url, output_file)
+
+
+async def fetch_location(session: ClientSession, site_id: int) -> None:
+    url, output_file = generate_url_for(site_id)
+    logger.info("getching site id: " + str(site_id))
+    async with session.get(url) as response:
+        contents = await response.text()
+        with output_file.open("w") as f:
+            f.write(contents)
+            f.write("\n")
+
+
+async def main(argv):
+    async with ClientSession() as session:
+        # fetch the baseurl to get a list of ids
+        async with session.get(base_url) as response:
+            contents = await response.json()
+
+            valid_ids = [o.get("id") for o in contents]
+            # valid_ids.sort()
+
+            futures = [fetch_location(session, site_id) for site_id in valid_ids]
+            await asyncio.gather(*futures)
+
+
+# If this file is being run from the CLI instead of imported as a module
+if __name__ == "__main__":
+    # discard the first item in sys.argv as it's the script name.
+    # Example: '.../fetch.py'
+    argv = sys.argv
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(argv))
