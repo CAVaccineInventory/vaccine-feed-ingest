110f7ce443489c285dfbd683151531e8c4c5e843
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-06-11 16:55:29 -0700

    remove unused imports

--- vaccine_feed_ingest/runners/la/dph/normalize.py
+++ vaccine_feed_ingest/runners/la/dph/normalize.py
@@ -9,14 +9,12 @@ from typing import List, Optional, OrderedDict
 
 import us
 import usaddress
-from opening_hours import OpeningHours
 from vaccine_feed_ingest_schema import location as schema
 
 from vaccine_feed_ingest.utils.log import getLogger
 from vaccine_feed_ingest.utils.normalize import (
     normalize_address,
     normalize_phone,
-    normalize_zip,
     parse_address,
 )
 
