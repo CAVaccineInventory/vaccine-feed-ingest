21032001391e31777baf4d1675b7b55a4b4ae6bb
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-06-17 20:12:12 -0700

    add parser for il state

---
+++ vaccine_feed_ingest/runners/il/state/parse.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import csv
+import json
+import pathlib
+import sys
+
+output_dir = pathlib.Path(sys.argv[1])
+input_dir = pathlib.Path(sys.argv[2])
+
+csv_filepath = input_dir / "il_state.csv"
+
+locations = []
+
+
+def transpose_keys(input_dict):
+    keymap = {
+        "Site Name": "name",
+        "Location Type": "loc_type",
+        "Address": "address",
+        "City": "address",
+        "Zip": "zip",
+        "County": "county",
+        "pindetails": "pindetails",
+        "LATITUDE": "lat",
+        "LONGITUDE": "long",
+        "Website": "website",
+    }
+
+    newdict = {}
+    for key, value in input_dict.items():
+        newdict[keymap[key]] = value
+
+    return newdict
+
+
+with open(csv_filepath) as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    for row in csvreader:
+        locations.append(transpose_keys(row))
+
+out_filepath = output_dir / "data.parsed.ndjson"
+
+with out_filepath.open("w") as fout:
+    for location in locations:
+        json.dump(location, fout)
+        fout.write("\n")
