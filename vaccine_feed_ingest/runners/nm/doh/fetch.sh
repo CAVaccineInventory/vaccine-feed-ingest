cc9dd480f512ab13ffc3a3dc20ada44a9af0caa5
curiousleo <curiousleo@users.noreply.github.com>
2021-06-20 22:15:11 +0200

    Comment

--- vaccine_feed_ingest/runners/nm/doh/fetch.sh
+++ vaccine_feed_ingest/runners/nm/doh/fetch.sh
@@ -2,16 +2,20 @@
 
 set -Eeuo pipefail
 
-output_dir=""
-
-if [ -n "${1}" ]; then
-    output_dir="${1}"
-else
-    echo "Must pass an output_dir as first argument"
+if [[ -z ${1+x} ]]; then
+    echo 'Must pass an output_dir as first argument'
+    exit 64
 fi
+output_dir="${1}"
 
 today=$(date --iso-8601=date)
 for d in $(seq 0 14); do
+    # We could just use a relative date here, but that could give surprising
+    # results if the script started just before midnight and ran into the
+    # following day.
+    #
+    # We circumvent this by using "today", set before fetching begins, as the
+    # reference date.
     date=$(date --iso-8601=date --date="${today} + ${d} days")
     curl \
         --silent \
